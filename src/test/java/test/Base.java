384abb006565c362eef23207b8671ed9b95983c5
chandra-ashish <60462561+chandra-ashish@users.noreply.github.com>
2020-07-28 13:25:20 +0530

    Update Base.java
    testing webhook

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -31,7 +31,7 @@ class Base {
 
     protected void run() {
         double r = Math.random();
-        String str1;
+        String str;
         if (r < 0.1) {
             fail("oops");
         } else if (r < 0.2) {
